c0c6274beee610d155c625ff7d432cdf68d3c4b7
age-18 <traffyou777@gmail.com>
2024-06-29 22:38:04 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('yesBtn').addEventListener('click', function() {
-    // ссылка yes
-    window.location.href = 'https://www.google.com.ua/';
-});
-
-document.getElementById('noBtn').addEventListener('click', function() {
-    // ссылка no
-    window.location.href = 'https://www.google.com.ua/';
-});

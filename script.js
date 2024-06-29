3e07318c89ca0f68a0cf51acf2ad7c7a3da51906
age-18 <traffyou777@gmail.com>
2024-06-29 21:29:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.getElementById('yesBtn').addEventListener('click', function() {
     // ссылка yes
-    window.location.href = '';
+    window.location.href = 'https://www.google.com.ua/';
 });
 
 document.getElementById('noBtn').addEventListener('click', function() {
     // ссылка no
-    window.location.href = '';
+    window.location.href = 'https://www.google.com.ua/';
 });

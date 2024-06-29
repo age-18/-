11d112a5dc89fb90eb5c85355985ec5719cff356
age-18 <traffyou777@gmail.com>
2024-06-29 21:20:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('yesBtn').addEventListener('click', function() {
+    // ссылка yes
+    window.location.href = '';
+});
+
+document.getElementById('noBtn').addEventListener('click', function() {
+    // ссылка no
+    window.location.href = '';
+});

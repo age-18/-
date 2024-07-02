2017e2f880116b53ecd04aa02444bfb0562e17c4
age-18 <traffyou777@gmail.com>
2024-07-02 18:48:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.getElementById('yesBtn').addEventListener('click', function() {
     // ссылка yes
-    window.location.href = '';
+    window.location.href = 'https://nutra-pro.online/dMulvO26?sub_id_1={site}&sub_id_2={camp}&sub_id_3={feed}&sub_id_4={click_id}';
 });
 
 document.getElementById('noBtn').addEventListener('click', function() {
     // ссылка no
-    window.location.href = '';
+    window.location.href = 'https://nutra-pro.online/dMulvO26?sub_id_1={site}&sub_id_2={camp}&sub_id_3={feed}&sub_id_4={click_id}';
 });
